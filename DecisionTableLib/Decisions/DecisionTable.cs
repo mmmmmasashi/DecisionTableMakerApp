61af8bb93fadda3f0b286496f48fbcce66b4808b
m_masashi <drift_tms@yahoo.co.jp>
2025-05-06 17:55:22 +0900

    refactoring : 並び順を整える等

--- DecisionTableLib/Decisions/DecisionTable.cs
+++ DecisionTableLib/Decisions/DecisionTable.cs
@@ -11,29 +11,15 @@ namespace DecisionTableLib.Decisions
     /// </summary>
     public class DecisionTable
     {
+        /// <summary>
+        /// テストケースのリスト
+        /// </summary>
         internal List<TestCase> TestCases { get; set; }
+
+        /// <summary>
+        /// 因子のリスト
+        /// </summary>
         public List<Factor> Factors { get => CreateFactors(); }
-
-        private List<Factor> CreateFactors()
-        {
-            var factorNames = TestCases.SelectMany(tc => tc.FactorNames).Distinct();
-
-            //因子名のリスト -> (因子 1-- * 水準)のリストに変換してFactorクラスにする
-            var factors = factorNames.Select(factorName =>
-            {
-                var levels = new List<Level>();
-                foreach(var testCase in TestCases)
-                {
-                    if (!testCase.HasFactorOf(factorName)) continue;
-                    var level = testCase.LevelOf(factorName);
-                    if (levels.Contains(level)) continue;
-                    levels.Add(level);
-                }
-                return new Factor(factorName, levels);
-            }).ToList();
-
-            return factors;
-        }
 
         /// <summary>
         /// 決定表のコンストラクタ
@@ -51,6 +37,27 @@ namespace DecisionTableLib.Decisions
                 sb.AppendLine(testCase.ToString());
             }
             return sb.ToString().TrimEnd('\n', '\r'); // 最後の改行を削除
+        }
+
+        private List<Factor> CreateFactors()
+        {
+            var factorNames = TestCases.SelectMany(tc => tc.FactorNames).Distinct();
+
+            //因子名のリスト -> (因子 1-- * 水準)のリストに変換してFactorクラスにする
+            var factors = factorNames.Select(factorName =>
+            {
+                var levels = new List<Level>();
+                foreach (var testCase in TestCases)
+                {
+                    if (!testCase.HasFactorOf(factorName)) continue;
+                    var level = testCase.LevelOf(factorName);
+                    if (levels.Contains(level)) continue;
+                    levels.Add(level);
+                }
+                return new Factor(factorName, levels);
+            }).ToList();
+
+            return factors;
         }
     }
 }
